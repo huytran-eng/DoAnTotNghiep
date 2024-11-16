98562fe98505baf0e5c4eeabc18b7b476c496087
ThanhHoang2002 <thanhhoang11102002@gmail.com>
2024-11-16 22:25:51 +0700

    complete teacher's home page

---
+++ front-end/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+import { Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes'; // Import các route
+
+const App = () => {
+  return (
+    <Routes>
+      {publicRoutes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+    </Routes>
+  );
+};
+
+export default App;
---
+++ front-end/src/components/Layout/DefaultLayout/Sidebar/index.jsx
@@ -0,0 +1,96 @@
+// src/components/Sidebar.jsx
+// import { Link } from 'react-router-dom';
+// import "../../../../styles/sidebarStyles.css";
+
+// const Sidebar = ( onShowContent ) => {
+//     return (
+//       <div className="sidebar">
+        
+//         <ul className="sidebar-list">
+//           <li className="sidebar-item">
+//             <Link to="/monhoc" onClick={onShowContent}>Môn Học</Link>
+//           </li>
+//           <li className="sidebar-item">
+//             <Link to="/khoahoc" onClick={onShowContent}>Khoa Học</Link>
+//           </li>
+//           <li className="sidebar-item">
+//             <Link to="/lophoc" onClick={onShowContent}>Lớp Học</Link>
+//           </li>
+//         </ul>
+//       </div>
+//     );
+// };
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+} from "@material-tailwind/react";
+import {
+  PresentationChartBarIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+  PowerIcon,
+  BookmarkIcon,
+  CodeBracketIcon,
+} from "@heroicons/react/24/solid";
+import  logoPtit from "../../../../assets/image/logo/Logo_PTIT.jpg";
+import { useNavigate } from "react-router-dom";
+export function Sidebar() {
+  const navigate = useNavigate();
+  return (
+    <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+      <div className="mb-2 p-4 flex justify-center items-center">
+        <img src={logoPtit} alt="logo" className="w-20 h-20" />
+      </div>
+      <List>
+        <ListItem onClick={() => navigate("/monhoc")}>
+          <ListItemPrefix>
+            <PresentationChartBarIcon className="h-5 w-5" />
+          </ListItemPrefix>
+           <Typography color="blue-gray" className="mr-auto font-normal">
+                Môn học
+          </Typography>
+        </ListItem>
+        <ListItem onClick={()=> navigate("/khoahoc")}>
+          <ListItemPrefix>
+            <BookmarkIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          <Typography color="blue-gray" className="mr-auto font-normal">
+            Khoa học
+          </Typography>
+        </ListItem>
+        <ListItem onClick={()=> navigate("/lophoc")}>
+          <ListItemPrefix>
+            <CodeBracketIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          <Typography color="blue-gray" className="mr-auto font-normal">
+            Lớp học
+          </Typography>
+        </ListItem>
+        <hr className="my-2 border-blue-gray-50" />
+        <ListItem>
+          <ListItemPrefix>
+            <UserCircleIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Profile
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <Cog6ToothIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Settings
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <PowerIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Log Out
+        </ListItem>
+      </List>
+    </Card>
+  );
+}
+
+export default Sidebar;
