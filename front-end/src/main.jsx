ad7e30a7f858c4650a23ac9729811fa752032e13
Nguyen Thanh Hoang <hoangnt@hivetech.vn>
2024-12-12 21:21:42 +0700

    fix to deploy

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './main.css';
 import { ThemeProvider } from "@material-tailwind/react";
 const root = createRoot(document.getElementById('root'));
 root.render(
