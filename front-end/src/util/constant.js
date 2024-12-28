a0be19c01c23f1525902cac47ec7c0e0ee629439
k.huytr4n@gmail.com <k.huytr4n@gmail.com>
2024-12-28 19:32:06 +0700

    update deploy url

--- front-end/src/util/constant.js
+++ front-end/src/util/constant.js
@@ -1,3 +1,3 @@
-// export const baseUrl = '/api/';
+export const baseUrl = '/api/';
 //export const baseUrl = 'http://36.50.135.228:5000/api/'
-export const baseUrl = 'https://localhost:7104/api/'
+//export const baseUrl = 'https://localhost:7104/api/'
